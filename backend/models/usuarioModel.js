c831460e8754de93b550e9253d2d29dc5556e60c
PabloAngeloNs <pabloangelons.06@gmail.com>
2025-06-11 20:39:38 -0300

    Inicindo db

---
+++ backend/models/usuarioModel.js
@@ -0,0 +1,22 @@
+class Usuario  {
+    constructor ({id, nome, email, password, historicoPontuacoes}) {
+        this.id = id
+        this.nome = nome
+        this.email = email
+        this.password = password
+        this.historicoPontuacoes = historicoPontuacoes
+    };
+
+
+    toJson() {
+        return{
+            id: this.id,
+            nome: this.nome,
+            email: this.email,
+            password: this.password,
+            historicoPontuacao: this.historicoPontuacoes
+        };
+    }
+}
+
+module.exports =  Usuario;
