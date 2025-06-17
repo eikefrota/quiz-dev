3e1c0984c682c7588c21d4f764325612464657b6
Benedito-Dev <beneditobittencourtt@gmail.com>
2025-06-17 20:33:55 -0300

    Remove node_modules do controle do Git

---
+++ mobile/app/components/tempo.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react'; 
+import { SafeAreaView, View, Text, StyleSheet } from 'react-native';
+import TimeComponent from './components/TimeComponent'; 
+
+const App = () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <TimeComponent />
+    </SafeAreaView>
+  );
+};
+
+const TimeComponent = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(timerId); // Limpeza do intervalo
+  }, []); // Array vazio para rodar apenas uma vez
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.timeText}>
+        {time.toLocaleTimeString()} {/* Exibe a hora */}
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  timeText: {
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+});
+
+export default TimeComponent;
