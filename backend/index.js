7f8b134313032aedef9a922ec2e3ab1802f30f2d
PabloAngeloNs <pabloangelons.06@gmail.com>
2025-07-02 18:48:53 -0300

    wip

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,5 @@
 const Server = require('./server');
 
-let serverApp;
 
-module.exports = async (req, res) => {
-  if (!serverApp) {
-    const server = new Server();
-    await server.initDb();
-    serverApp = server.app;
-  }
-  return serverApp(req, res);
-};
+const server = new Server();
+server.start();
